d95ba4ab0db895ad61a69c53b42f04ebf7e8ba10
ziyuan18 <47776767+ziyuan18@users.noreply.github.com>
2019-05-22 20:57:13 +0800

    Code cleanup based on pull requests

--- src/main/java/ai/preferred/crawler/eventbrite/master/EventListParser.java
+++ src/main/java/ai/preferred/crawler/eventbrite/master/EventListParser.java
@@ -1,10 +1,13 @@
-package ai.preferred.crawler.eventbrite.master;
+ package ai.preferred.crawler.eventbrite.master;
 
 import ai.preferred.crawler.eventbrite.entity.EBEvent;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ai.preferred.venom.job.Scheduler;
+import ai.preferred.venom.request.Request;
 import ai.preferred.venom.request.VRequest;
+import ai.preferred.venom.response.VResponse;
 import org.jsoup.*; 
 import org.jsoup.nodes.*; 
 import org.jsoup.select.*; 
@@ -15,21 +18,19 @@ import java.io.*;
 import java.util.*;
 
 public class EventListParser {
-	
-	static List<EBEvent> parseListing(Document document){
+
+	static void parseListing(Document document, Scheduler scheduler){
 		final Elements events = document.select("div[class=eds-show-down-mn eds-l-mar-top-4] li[data-reactid]");
-		final ArrayList<EBEvent> result = new ArrayList<>(events.size()); 
 		for(final Element eb : events) {
-			result.add(parseEvent(eb)); 
+			parseEvent(scheduler, eb); 
 		}
-		return result; 
 	}
 	
 	private static String textOrNull(Element element) {
 		return null == element ? null : element.text();
 	}
 	
-	private static EBEvent parseEvent(Element e){
+	private static void parseEvent(Scheduler scheduler, Element e){
 		final EBEvent event = new EBEvent(); 
 		event.setTitle(textOrNull(e.select("div[class=event-card__formatted-name--is-clamped]").first())); 
 		event.setDateTime(textOrNull(e.select("div[class=eds-media-card-content__sub-content] > div[class=eds-text-bs--fixed eds-text-color--grey-600 eds-l-mar-top-1]").first())); 
@@ -37,15 +38,7 @@ public class EventListParser {
 		Element link = e.select("a[class=eds-media-card-content__action-link]").first();
 		String url = link.attr("href"); 
 		event.setUrl(url); 
-		//return event; 
-		try{
-			Document descDocument = Jsoup.connect(url).get(); 
-			//System.out.println(descDocument.body().text()); 
-			Element descElement = descDocument.selectFirst("div[data-automation=listing-event-description]"); 
-			event.setDescription(textOrNull(descElement)); 
-			return event; 
-		} catch (IOException ioe){
-			return event; 
-		}
+		scheduler.add(new VRequest(url), new EventHandler(event)); 
 	}
+	
 }
